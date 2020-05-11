0b7dc449f7169639ed4afed4cce2046110aac75c
Koan Loc <loc.koan@yahoo.com>
2020-05-10 23:55:35 -0500

    time block colors

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 // location of scripts
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-$("#currentDay").text(moment().format());
+
+//$("#currentDay").text(moment().format("YYYY MMMM Do"));/* for moment to work, it had to be referneced in this file too */
 //document.getElementById("currentDay").innerHTML = Date();
 //document.write(Date());  -- write will overwrite entire document
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> */}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" 
+integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" 
+crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
