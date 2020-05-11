99c6477861eff61c5eb3042832c364939cbc83c2
Koan Loc <loc.koan@yahoo.com>
2020-05-11 16:18:01 -0500

    edits

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
 $(document).ready(function () { /* adding this makes sure javascript fully checked/ran before executing */
 
-  // adding date under heading
-  $("#currentDay").text(moment().format("YYYY MMMM Do")); /* moment website / document was painfully long*/ 
+    // adding date under heading
+    $("#currentDay").text(moment().format("YYYY MMMM Do")); /* moment website / document was painfully long*/ 
 
-  // check what moment spits out and is working
-  console.log(moment());
-  console.log(typeof(moment()));
+    // check what moment spits out and is working
+    console.log(moment());
+    console.log(typeof(moment()));
+
+    var clock;
+    var currentTime = moment();
+    var currentHour = moment().hour(); /* time changes so should be var*/
+    var currentMin = moment().minutes(); 
+    console.log(currentHour); /* tells current hour */
+    console.log(currentMin); /* tells current minutes */
+
+    // color code timeblocks
+	$(".time-block").each(function() {
+	  
+        // var currentTime = moment().hours();
+        var hourBlock = parseInt($(this).attr("id"));
   
-  var currentTime = moment().hour(); /* time changes so should be var*/
-  console.log(currentTime); /* tells current hour */
-
-  // color code timeblocks
+        if (currentHour > hourBlock) { /* odd that after $ classes needs . but after addClass, it does not need . */
+          $(".description").addClass("past"); 
+        } else if (currentHour === hourBlock) {
+          $(".description").addClass("present");
+        } else {
+          $(".description").addClass("future");
+        }
   
-//   function timeReview () {
-//     if(currentTime.isBefore(currentTime)){
-//       xxxxx.attr("class", "past");
-//     } else if (xxxx === currentHour) {
-//       xxxxx.attr("class", "present");
-//     } else (xxxxx.isAfter(xxxxx)) {
-//       xxxxx.attr("class", "future");
-//     }
-//   }
-//   timeReview(); /* runs function */
+      })
 
-  // save buttons
-//   $(".saveBtn").click(function() { /* click not onclick */
-//     var timeArea = $(this).parent().attr("id");
-//     var enteredText = $(this).siblings(".description").val();
-//     localStorage.setItem("timeArea", "enteredText"); /* in between parenthesis is name/key and its value */
+    // save buttons
+    //   $(".saveBtn").click(function() { /* click not onclick */
+    //     var timeArea = $(this).parent().attr("id");
+    //     var enteredText = $(this).siblings(".description").val();
+    //     localStorage.setItem("timeArea", "enteredText"); /* in between parenthesis is name/key and its value */
 
-//   })
+    //   })
 
-  // localStorage.setItem(, JSON.stringify(events));
+    // localStorage.setItem(, JSON.stringify(events));
 });
